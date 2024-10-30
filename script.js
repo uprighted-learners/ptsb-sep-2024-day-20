ee2166d88a6d0a3de0a5c030d6ff3dae2a9e1c73
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-30 18:28:56 -0500

    more monster objects

--- script.js
+++ script.js
@@ -14,7 +14,39 @@ let monster = {
         console.log(`Bolt of lighning streaks across the sky!`)
         this.health *= 2;
         console.log(`${this.name} has ${this.health} health!`);
-    }
+    },
 }
 
 monster.bolt();
+
+// monster.bolt = () => {
+//     console.log(`Bolt of lighning streaks across the sky!`)
+//     this.health *= 3;
+//     console.log(`${this.name} has ${this.health} health!`);
+// }
+
+// monster.bolt();
+
+let witch = {
+    name: "Agatha Harkness",
+    potions: ["elixir of fear", "love tincture", "healing potion"],
+    spells: ["fireball", "lightning bolt", "ice shard"],
+
+    cackle() {
+        console.log("Heheheheheheheh!");
+    },
+
+    castSpell(spell) {
+        console.log(`${this.name} casts ${this.spells[spell]}!`);
+        this.cackle();
+    },
+
+    drinkPotion(potion) {
+        console.log(`${this.name} drinks ${this.potions[potion]}!`);
+    }
+}
+
+witch.castSpell(0);
+witch.drinkPotion(0);
+
+console.log(witch.name)
