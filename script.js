ba46dbbdb1176baffd697158dd2916241487af90
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-30 17:50:10 -0500

    object methods

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+let monster = {
+    name: "Frank the Frankenstein",
+    health: 100,
+    roar() {
+        console.log("ROAR!!!");
+    },
+    attack() {
+        console.log(`You attack ${this.name} for 10 damage!`);
+        this.roar();
+        monster.health -= 10;
+        console.log(`${this.name} has ${this.health} health left!`);
+    },
+    bolt() {
+        console.log(`Bolt of lighning streaks across the sky!`)
+        this.health *= 2;
+        console.log(`${this.name} has ${this.health} health!`);
+    }
+}
+
+monster.bolt();
